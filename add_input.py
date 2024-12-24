bda9b6ae47174c3d1cb2675880bdf7e7630fc700
TikhonPiminov <tihon.piminov@yandex.com>
2024-12-24 19:58:08 +0300

    Add files via upload
    Grade 1. Этап 1: Задание 3

---
+++ add_input.py
@@ -0,0 +1,6 @@
+note_name = input("Название заметки: ")
+date_d = input("День:")
+date_m = input("Месяц: ")
+date_y = input("Года: ")
+print(f"Заметка: {note_name}"
+      f" Дата создания заметки: {date_d }-{date_m}-{date_y}")
