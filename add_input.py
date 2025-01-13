f82a47b4c7cf57eb26dfbe26e74f3cf8f6002290
TikhonPiminov <tihon.piminov@yandex.com>
2025-01-13 21:10:09 +0300

    Update add_input.py

--- add_input.py
+++ add_input.py
@@ -1,6 +1,4 @@
 note_name = input("Название заметки: ")
-date_d = input("День:")
-date_m = input("Месяц: ")
-date_y = input("Года: ")
+date_ = input("Введите дату в формате (дд-мм-гггг)")
 print(f"Заметка: {note_name}"
       f" Дата создания заметки: {date_d }-{date_m}-{date_y}")
