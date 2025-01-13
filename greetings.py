5a4768316f39d87c224583e70e0c07ef68b1b2fd
TikhonPiminov <tihon.piminov@yandex.com>
2025-01-13 21:18:07 +0300

    Update greetings.py

--- greetings.py
+++ greetings.py
@@ -1,6 +1,7 @@
-username = Tikhon
-title = Grade 1. Stage 1: Task 1
-content = a project to create a journal with notes
-status = development
-created_date = 23.12.2024
-issue_date = 17.06.2025
+username = "Tikhon"
+name_notes = "Grade 1. Stage 1: Task 1"
+content = "a project to create a journal with notes"
+status = "development"
+created_date = "23.12.2024"
+issue_date = "17.06.2025"
+print(f"Никнем:{username} , название заметки :{name_notes} , дэдлайн: {issue_date})
