3e6dbc813a2623b377ec208fa8a05881ea4ade13
TikhonPiminov <tihon.piminov@yandex.com>
2025-01-28 19:17:53 +0300

    Update and rename 1.2.4 multiple_notes.py..py to 1.2.4 multiple_notes.py

--- 1.2.4 multiple_notes.py
+++ 1.2.4 multiple_notes.py
@@ -4,21 +4,22 @@ notes = []
 def new_note () :
     note = {                   #информация о заметке ввод
         "user_name": input("Ваш логин:"),
-        "name_note": input(f"Название заметки : "),
+        "name_note": input(f"Заголвок заметки: "),
         "description_note": input("Описание заметки: "),
-        "deadline_note": input("Дэдлан заметки (дд-мм-гггг): "),
+        "data_note": input("Дата создания (дд-мм-гггг): "),
+        "deadline_note": input("Дэдлан заметки (дд-мм-гггг): ")
+
 
             }
 
-    return note
+    return notes
 
 
 while True :           # возможные действия с заметкой
     print("Выберите действие; \n" 
           "1 - Новая заметка; \n"
           "2 - Просмотреть все заметки; \n"
-          "3 - Удалить заметку; \n"
-          "4 - Выйти; \n"
+          "3 - Выйти; \n"
           )
     operator = input("Введите название операции: ") # ввод выбранной операвции
     if operator == "1":
@@ -27,7 +28,7 @@ while True :           # возможные действия с заметкой
     elif operator == "2":
         print(notes)
     elif operator == "3":
-        notes.remove(note_remov())
-    elif operator == "4":
         print(" Выход.")
         break
+
+
